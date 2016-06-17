d2209a453071eb3a9523d080a485bc6633b15055
ashleyraymaceli <ashley.maceli@gmail.com>
2016-06-17 14:49:39 -0700

    add click actions for star rating selection, add computed property and custom helper to display stars in rating-tile

--- app/components/new-rating.js
+++ app/components/new-rating.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 import moment from 'moment';
 
+var clearStars = function() {
+  $('.star-rating-selection').removeClass('selected-stars');
+}
+
 export default Ember.Component.extend({
   actions: {
     saveRating() {
@@ -15,7 +19,32 @@ export default Ember.Component.extend({
       this.set('username', "");
       this.set('stars', "");
       this.set('comment', "");
+      clearStars();
     },
-
+    rateOneStar() {
+      clearStars();
+      this.set('stars', 1);
+      $('#rateOneStar').addClass('selected-stars');
+    },
+    rateTwoStar() {
+      clearStars();
+      this.set('stars', 2);
+      $('#rateTwoStar').addClass('selected-stars');
+    },
+    rateThreeStar() {
+      clearStars();
+      this.set('stars', 3);
+      $('#rateThreeStar').addClass('selected-stars');
+    },
+    rateFourStar() {
+      clearStars();
+      this.set('stars', 4);
+      $('#rateFourStar').addClass('selected-stars');
+    },
+    rateFiveStar() {
+      clearStars();
+      this.set('stars', 5);
+      $('#rateFiveStar').addClass('selected-stars');
+    },
   }
 });
--- app/components/rating-tile.js
+++ app/components/rating-tile.js
@@ -1,5 +1,55 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  displayStarRating: Ember.computed('rating.stars', function() {
+    if (this.get('rating.stars') === 1) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    } else if (this.get('rating.stars') === 2) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    } else if (this.get('rating.stars') === 3) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    } else if (this.get('rating.stars') === 4) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    } else if (this.get('rating.stars') === 5) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>'
+      );
+    } else {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    }
+  }),
 });
