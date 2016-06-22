5e146382b5fa376d1f585a059d43e60b67afc5d9
ashleyraymaceli <ashley.maceli@gmail.com>
2016-06-21 18:38:12 -0700

    update averageRating computed property to fix asynchronous problem

--- app/components/average-rating.js
+++ app/components/average-rating.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  averageRating: Ember.computed('hike', function() {
+  averageRating: Ember.computed('hike.ratings.@each.stars', function() {
     var totalSum = 0;
     var totalRatings = this.get('hike.ratings.length');
     this.get('hike.ratings').forEach(function(hike) {
